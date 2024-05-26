76cfc3247d9fc5d966637fd41fe1c1238bffa6ec
casey_primeecosystem <casey@primeecosystem.com.au>
2024-05-26 13:06:51 +1000

    Add eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Boston');
+import express from 'express';
+
+const app = express();
+const port = Number(process.env.PORT || 3000);
+
+app.get('/', (req, res) => {
+  res.send('Boston 🐶 is running!');
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
